627193360cf25a4f43dc0b506abfa31e17a03042
delola\delola lokal <aleksandra.man@gmail.com>
2017-03-21 18:10:53 +0100

    corrected version

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,18 @@ $(document).ready(function() {
 	var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
 	function getQuote() {
-		$.getJSON(quoteUrl,
-			createTweet);
+		$.getJSON(quoteUrl,	createTweet);
 	}
 
 	function createTweet(input) {
-		var tweetText = "Quote of the day - " +
-            input.quoteText + " Author: " +
-            input.quoteAuthor;
+		var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
 
 		if (tweetText.length > 140) {
 			getQuote();
 		} else {
-			var tweet = tweetLink +
-                encodeURIComponent(tweetText);
+			var tweet = tweetLink + encodeURIComponent(tweetText);
 			$(".quote").text(input.quoteText);
-			$(".author").text("Author: " +
-                input.quoteAuthor);
+			$(".author").text("Author: " + input.quoteAuthor);
 			$(".tweet").attr("href", tweet);
 		}
 		if (!input.quoteAuthor.length) {
